3f1364b7bf91e6863396d012d3fda97776d13a41
strygul <strygul@gmail.com>
2016-05-18 15:33:06 +0300

    Calculator

---
+++ src/CalcApp.java
@@ -0,0 +1,118 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CalcApp extends Application {
+
+    private String str;
+    private TextField text;
+    private MyCalculator myCalc = new MyCalculator();
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage myStage) throws Exception {
+        myStage.setTitle("Mega Super Cool Calculator - 2016");
+        myStage.setScene(createScene());
+        myStage.show();
+
+    }
+
+    public Scene createScene() {
+        VBox rootNode = new VBox();
+        HBox resultBox = new HBox();
+
+        HBox functionsPanel = new HBox();
+        functionsPanel.setSpacing(4.0);
+
+        text = new TextField("0");
+        text.setMaxSize(172, 50);
+        text.setStyle("-fx-font:bold 20px Arial; -fx-alignment: CENTER_RIGHT");
+        text.setEditable(false); //should be false
+
+        //Columns
+        VBox columns[] = {new VBox(), new VBox(), new VBox(), new VBox()};
+
+        Button arrayOfButtons[] = {
+                new Button("7"), new Button("4"), new Button("1"), new Button("C"),
+                new Button("8"), new Button("5"), new Button("2"), new Button("0"),
+                new Button("9"), new Button("6"), new Button("3"), new Button("="),
+                new Button("+"), new Button("-"),new Button("/"), new Button("*")};
+
+        final EventHandler<ActionEvent> actionHandler = new EventHandler<ActionEvent>(){
+            @Override
+            final public void handle(ActionEvent actionEvent) {
+                str = actionEvent.toString();
+                char ch = str.charAt(str.length() - 3);
+                str = "" + ch;
+                if(str.compareTo("C") == 0) {
+                    text.setText("0");
+                    myCalc.reset();
+                } else {
+                    myCalc.inString(str);
+                    text.setText(myCalc.getOperationInString());
+                }
+            }
+        };
+
+        for(int i = 0; i < arrayOfButtons.length; i++) {
+            int j = i/4;
+
+            Button bttn = arrayOfButtons[i];
+
+            bttn.setMinSize(40, 40);
+            bttn.setStyle("-fx-margin:20px");
+            bttn.setOnAction(actionHandler);
+
+            if(i >= 11) bttn.setStyle("-fx-text-fill: blue");
+            if(i == 3) bttn.setStyle("-fx-text-fill: red");
+
+            columns[j].getChildren().add(bttn);
+        }
+
+        for(int i = 0; i < columns.length; i++) {
+            columns[i].setSpacing(4);
+            functionsPanel.getChildren().add(columns[i]);
+        }
+
+        final EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(final KeyEvent keyEvent) {
+                str = keyEvent.getCharacter();
+                System.out.println(str);
+                if(str.compareTo("c") == 0) {
+                    text.setText("0");
+                    myCalc.reset();
+                } else {
+                    myCalc.inString(str);
+                    if(myCalc.getOperationInString().compareTo(myCalc.byZero) == 0) {
+                        text.clear();
+                    } else {
+                        text.setText(myCalc.getOperationInString());
+                    }
+                }
+            }
+        };
+
+        text.setOnKeyTyped(keyEventHandler);
+        rootNode.getChildren().addAll(resultBox, functionsPanel);
+        resultBox.getChildren().add(text);
+
+
+        rootNode.setAlignment(Pos.CENTER);
+
+        return new Scene(rootNode);
+    }
+
+}
---
+++ src/MyCalculator.java
@@ -0,0 +1,81 @@
+public class MyCalculator {
+
+    private char lastOperation;
+    private String numberInString;
+    private String operationInString;
+    private int number;
+    private int result;
+    public final String byZero = "Error!";
+
+    public MyCalculator() {
+        numberInString = "";
+        operationInString = "";
+        lastOperation = 'z';
+    }
+
+    public void reset() {
+        numberInString = "";
+        lastOperation = 'z';
+        result = 0;
+
+        if(!operationInString.equals(byZero)) {
+            operationInString = "";
+        }
+    }
+
+    public void inString(String str) {
+        if(operationInString.equals(byZero)) {
+            operationInString = "";
+        }
+
+        if(str.matches("[0-9]")) {
+            numberInString += str;
+            operationInString += str;
+            parseLastNumberInString();
+        } else if(str.matches("[+-/*]")) {
+            numberInString = "";
+            executeOperation();
+            lastOperation = str.charAt(0);
+            if(!operationInString.equals(byZero)) {
+                operationInString += " " + str + " ";
+            } else {
+                reset();
+            }
+        } else if(str.compareTo("=") == 0){
+            numberInString = "";
+            executeOperation();
+            lastOperation = str.charAt(0);
+            if(!operationInString.equals(byZero)) {
+                operationInString = "" + result;
+            }
+        } else if(str.compareTo("c") == 0) {
+            reset();
+        }
+    }
+
+    private void executeOperation() {
+        switch(lastOperation) {
+            case '+': result += number; break;
+            case '-': result -= number; break;
+            case '*': result *= number; break;
+            case '/': {
+                if(number != 0) {
+                    result /= number;
+                } else {
+                    operationInString = byZero;
+                    reset();
+                }
+            } break;
+            case 'z': result = number; break;
+        }
+    }
+
+    private void parseLastNumberInString() {
+        number = Integer.parseInt(numberInString);
+    }
+
+    public String getOperationInString() {
+        return operationInString;
+    }
+
+}
